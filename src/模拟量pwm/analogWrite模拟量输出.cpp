6ca0bd870a577101ea97dd9529f7c2cb5c4545aa
yangyiko <yangyiko@163.com>
2021-01-19 17:17:01 +0800

    模拟量输出

---
+++ src/模拟量pwm/analogWrite模拟量输出.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+ int LED = D6;
+
+void setup() {
+  Serial.begin(115200);
+  
+}
+
+void loop() {
+  for (size_t brightness = 0; brightness < 255; brightness++)
+  {
+    analogWrite(LED,brightness);
+    Serial.println(brightness);
+    delay(10);
+  }
+}
