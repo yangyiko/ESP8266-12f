bb04d08391d43b30f89c19495bd6c9a0cc8e76f3
yangyiko <yangyiko@163.com>
2021-02-01 16:33:11 +0800

    3

--- src/DS18B20_单总线.cpp
+++ src/DS18B20_单总线.cpp
@@ -1,26 +1,26 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
+// #include <OneWire.h>
+// #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS D6
+// #define ONE_WIRE_BUS D6
 
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensor(&oneWire);
-
-
-void setup(void)
-{
-  Serial.begin(115200);
-  Serial.println(__FILE__);
-
-  sensor.begin();
-}
+// OneWire oneWire(ONE_WIRE_BUS);
+// DallasTemperature sensor(&oneWire);
 
 
-void loop(void)
-{
-  sensor.requestTemperatures();
-  Serial.println(sensor.getTempCByIndex(0));
-  delay(500);
-}
+// void setup(void)
+// {
+//   Serial.begin(115200);
+//   Serial.println(__FILE__);
+
+//   sensor.begin();
+// }
+
+
+// void loop(void)
+// {
+//   sensor.requestTemperatures();
+//   Serial.println(sensor.getTempCByIndex(0));
+//   delay(500);
+// }
